16c98509723f5ab20fdba3e317b332ccd0f01a1f
two_star <xingxin.hsing@gmail.com>
2017-09-30 21:23:45 +0800

    二分查找 递归

--- src/practice/BinarySearch.java
+++ src/practice/BinarySearch.java
@@ -1,13 +1,35 @@
 package practice;
 
+import java.util.Arrays;
+
 public class BinarySearch {
 	
 	/*=====================二分查找===========================================*/
+    /*-------递归---------*/
+	/**
+     * @param nums: The integer array.
+     * @param target: Target to find.
+     * @start: 0.
+     * @end: nums.length - 1.
+     */
+    public int binarySearchCur(int[] nums, int target, int start, int end) {
+    	if (start <= end) {
+    		int mid = start + (end-start) / 2;
+    		if (target == nums[mid]) {
+    			return mid;
+    		} else if (target > nums[mid]) {
+    			return binarySearchCur(nums, target, mid, end);
+    		} else if (target < nums[mid]) {
+    			return binarySearchCur(nums, target, start, mid);
+    		}
+    	}
+    	return -1;
+    }
+	
 	/*-------非递归，返回位置-----------------*/
 	/**
      * @param nums: The integer array.
      * @param target: Target to find.
-     * @return: The first position of target. Position starts from 0.
      */
 	 /*使用三个指针，分别指向头、尾、待比较的中间位置。*/
     public int binarySearch(int[] nums, int target) {
@@ -63,8 +85,12 @@ public class BinarySearch {
         return -1;  
     }
     
-    
     /*-------非递归，返回最后一次出现的位置---------*/
+	/**
+     * @param nums: The integer array.
+     * @param target: Target to find.
+     * @return: The last position of target. Position starts from 0.
+     */
     public int binarySearchLastLoc(int[] nums, int target) {
     	if (nums == null || nums.length == 0)
     		return -1;
@@ -85,5 +111,6 @@ public class BinarySearch {
     	}
     	return -1;
     }
+    
 
 }
