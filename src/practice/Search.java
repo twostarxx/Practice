2a8c1e3768f303e271bb4e5857b99c47a27b4f36
two_star <xingxin.hsing@gmail.com>
2017-09-30 20:03:54 +0800

    返回第一个位置

--- src/practice/Search.java
+++ src/practice/Search.java
@@ -3,13 +3,16 @@ package practice;
 public class Search {
 	
 	/*=====================二分查找===========================================*/
-	/*-------非递归---------*/
+	/*-------非递归，返回位置-----------------*/
 	/**
      * @param nums: The integer array.
      * @param target: Target to find.
      * @return: The first position of target. Position starts from 0.
      */
+	 /*使用三个指针，分别指向头、尾、待比较的中间位置。
+	   注意：指针修改时并不是直接改为middle。*/
     public int binarySearch(int[] nums, int target) {
+        //write your code here
         int low = 0;
         int high = nums.length-1;
         int middle;
@@ -24,6 +27,35 @@ public class Search {
             } 
         }
         return -1;
+    }
+	
+	
+	/*-------非递归，返回第一次出现的位置---------*/
+	/**
+     * @param nums: The integer array.
+     * @param target: Target to find.
+     * @return: The first position of target. Position starts from 0.
+     */
+    public int binarySearchFirstLoc(int[] nums, int target) {
+        if (nums == null || nums.length == 0) {  
+            return -1;  
+        }  
+        int start = 0, end = nums.length - 1;  
+        while (start + 1 < end) {   //区间长度小于2
+            int mid = start + (end - start) / 2;  
+            if (nums[mid] >= target) {  
+                end = mid;  
+            } else {  
+                start = mid;  
+            }  
+        }  
+        if (nums[start] == target) {  
+            return start;  
+        }  
+        if (nums[end] == target) {  
+            return end;  
+        }  
+        return -1;  
     }
 
 }
